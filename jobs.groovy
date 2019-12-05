b233960e120f18fce248b829f0982e9690330495
Alexey Gvozdev <Alexey_Gvozdev@epam.com>
2019-12-05 12:16:51 +0300

    Modify file jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,3 +1,6 @@
+def git_info = ("git ls-remote -h https://github.com/alekseygvozdev/d323dsl").execute()
+def branches = git_info.text.readLines().collect { it.split()[1].replaceAll('refs/heads/', '')}.unique()
+
 job('MNTLAB-agvozdev-main-build-job') {
 	parameters {
 		choiceParam('BRANCH_NAME', ['agvozdev', 'master'], 'Branch name')
@@ -5,11 +8,8 @@ job('MNTLAB-agvozdev-main-build-job') {
 			description('Choose jobs')
 			choiceType('CHECKBOX')
 			groovyScript {
-				script('''return ["MNTLAB-agvozdev-child1-build-job", 
-                       "MNTLAB-agvozdev-child2-build-job", 
-                       "MNTLAB-agvozdev-child3-build-job", 
-                       "MNTLAB-agvozdev-child4-build-job"]''')
-					     }
+              script('list = []; for(i in 1..4){ list.add("MNTLAB-agvozdev-child${i}-build-job")}; return list\n')
+				}
 			}
 		}
 		steps {
@@ -21,31 +21,45 @@ job('MNTLAB-agvozdev-main-build-job') {
 					failure("FAILURE")
 				}
                 parameters {
-                    predefinedProp('BRANCH_NAME', '$BRANCH_NAME')
+                predefinedProp('BRANCH_NAME', '${BRANCH_NAME}')
+              	currentBuild()	
                 }
             }
         }
+
     }
 }
 for (i in 1..4) {
-	job("MNTLAB-agvozdev-child${i}-build-job") {
+  job("MNTLAB-agvozdev-child${i}-build-job") {
+    	parameters {      
+			choiceParam('BRANCH_NAME', branches, 'Choose a branch')
+    }
+          
 		scm {
 			git {
 				remote {
 					github("alekseygvozdev/d323dsl", "https")
 				}
-				branch('$BRANCH_NAME')
+              branch('$BRANCH_NAME')
 				}
 			}
+      	logRotator {
+        numToKeep(5)
+        artifactNumToKeep(1)
+    }
+      	wrappers {
+        	preBuildCleanup {
+            deleteDirectories(false)
+            cleanupParameter()
+        	}
+        }
 		steps {
-            shell('''chmod +x script.sh
-                ./script.sh > output.txt
-				tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy''')
+            shell('chmod +x script.sh; ./script.sh > output.txt; tar -czvf ${BRANCH_NAME}_dsl_script.tar.gz jobs.groovy')
         }
      	publishers {
 			archiveArtifacts {
               pattern('${BRANCH_NAME}_dsl_script.tar.gz')
             }
         }     
-    }  
+    }
 }
